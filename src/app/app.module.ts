2b1e02b3086a451d11735b4d4fa51ca5ab490415
ba11breaker <15212219417@163.com>
2020-07-10 23:23:27 +1000

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { CoreModule } from './core/core.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CoreModule,
+    MatSidenavModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/core/core.module.ts
@@ -0,0 +1,30 @@
+import { NgModule, SkipSelf, Optional } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+
+
+
+@NgModule({
+  declarations: [
+    HeaderComponent, 
+    FooterComponent, 
+    SidebarComponent,
+  ],
+  imports: [
+    CommonModule,
+  ],
+  exports: [
+    HeaderComponent, 
+    FooterComponent, 
+    SidebarComponent,
+  ],
+})
+export class CoreModule {
+  constructor(@Optional() @SkipSelf() parent: CoreModule) {
+    if (parent) {
+      throw new Error('Module exits, could not reload it! ');
+    }
+  }
+ }
