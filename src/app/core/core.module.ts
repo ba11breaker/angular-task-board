62d4e1f9e3cccec81eea067b61c9b00006b0c836
ba11breaker <15212219417@163.com>
2020-07-11 01:29:13 +1000

    update

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,10 +1,9 @@
 import { NgModule, SkipSelf, Optional } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { SharedModule } from '../shared/shared.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
-
-
 
 @NgModule({
   declarations: [
@@ -13,7 +12,8 @@ import { SidebarComponent } from './sidebar/sidebar.component';
     SidebarComponent,
   ],
   imports: [
-    CommonModule,
+    SharedModule,
+    BrowserAnimationsModule,
   ],
   exports: [
     HeaderComponent, 
@@ -22,7 +22,9 @@ import { SidebarComponent } from './sidebar/sidebar.component';
   ],
 })
 export class CoreModule {
-  constructor(@Optional() @SkipSelf() parent: CoreModule) {
+  constructor(
+    @Optional() @SkipSelf() parent: CoreModule
+    ) {
     if (parent) {
       throw new Error('Module exits, could not reload it! ');
     }
